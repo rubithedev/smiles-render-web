43550e2e2031512e9024bfc056aca0a065f948e7
Gabe <gabrielgrechuk@outlook.com>
2025-11-30 18:29:59 -0300

    #40 - New front-end implementation scafolding

--- src/frontend/index.tsx
+++
@@ -1,6 +0,0 @@
-import * as ReactDOM from 'react-dom/client';
-import React from 'react';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(<App />);
